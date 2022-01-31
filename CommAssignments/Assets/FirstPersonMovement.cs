18e01ef7b418c99d32040a191553ce4317fa20bb
stlouisj3 <stlouisj3@yahoo.com>
2022-01-31 14:33:24 -0600

    Jump Added

--- CommAssignments/Assets/FirstPersonMovement.cs
+++ CommAssignments/Assets/FirstPersonMovement.cs
@@ -8,6 +8,7 @@ public class FirstPersonMovement : MonoBehaviour
 
     public float speed = 12f;
     public float gravity = -9.8f;
+    public float jumpHeight = 3f;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
@@ -22,7 +23,7 @@ public class FirstPersonMovement : MonoBehaviour
 
         if(isGrounded && velocity.y < 0)
         {
-            velocity.y = 2f;
+            velocity.y = -2f;
         }
 
         float x = Input.GetAxis("Horizontal");
@@ -31,6 +32,11 @@ public class FirstPersonMovement : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
 
         velocity.y += gravity * Time.deltaTime;
 
